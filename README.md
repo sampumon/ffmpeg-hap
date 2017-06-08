a37ceed71b336e634fdf81f2b5c83f50ef831e64
Samuli Kaipiainen <samuli@viiksipojat.fi>
2017-06-08 19:59:11 +0300

    we have output filename template!

--- README.md
+++ README.md
@@ -6,7 +6,17 @@ we'll convert ur *.{mov,mp4,m4v} recursively into [*_hap.mov](http://vdmx.vidvox
 USAGE
 -----
 
-	ffmpeg-hap directory
+	ffmpeg-hap directory ['output_template.mov']
+
+these are the components you can inject into _output_template_. they're bash variables, so escape with ' 😅
+
+	INPUT lol/cats/tiger.h246.mov
+	$path lol/cats
+	$name          tiger.h264.mov
+	$base          tiger.h264
+	$ext                      mov
+
+default: `$path/${base}_hap.mov` → `lol/cats/tiger.h246_hap.mov`
 
 homebrew note
 -------------
--- ffmpeg-hap.sh
+++ ffmpeg-hap.sh
@@ -1,18 +1,37 @@
 [[ ! "$1" ]] && {
-	echo "convert *.{mov,mp4,m4v} recursively into hap"
-	echo "output is same directory as input"
-	echo "usage: $0 directory"
+	cat <<-TAC
+	convert *.{mov,mp4,m4v} recursively into hap.
+
+	usage: $0 directory ['output_template.mov']
+
+	here's what components you can inject in output_template
+	  INPUT lol/cats/tiger.h246.mov
+	  \$path lol/cats
+	  \$name          tiger.h264.mov
+	  \$base          tiger.h264
+	  \$ext                      mov
+
+	default: \$path/\${base}_hap.mov → lol/cats/tiger.h246_hap.mov
+
+	GOOD~LUCK~~!
+	TAC
 	exit 1
 }
 
 # process one movie
 do_ffmpeg () {
-	dirplusbase=${1%.*}
-	filename=${1##*/}
-	out=${dirplusbase}_hap.mov
+	path=${1%/*}
+	name=${1##*/}
+	base=${name%.*}
+	ext=${name##*.}
+	eval out=$2
+
 	ffmpeg -i "$1" -c:v hap -an "$out"
 }
 
+# default output
+out=${2:-'$path/${base}_hap.mov'}
+
 # find *.mov
 export -f do_ffmpeg
-find "$1" \( -name "*.mov" -o -name "*.mp4" -o -name "*.m4v" \) -exec bash -c 'do_ffmpeg "{}"' \;
+find "$1" \( -name "*.mov" -o -name "*.mp4" -o -name "*.m4v" \) -exec bash -c "do_ffmpeg '{}' '$out'" \;
