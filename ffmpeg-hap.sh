6193c0cba7447cfa59421bcbff8f6ce9115fd60e
Samuli Kaipiainen <samuli@viiksipojat.fi>
2017-06-08 18:15:02 +0300

    poop ur {mov,m4v,mp4} in original directory

--- ffmpeg-hap.sh
+++ ffmpeg-hap.sh
@@ -1,16 +1,18 @@
 [[ ! "$1" ]] && {
-	echo "convert *.mov recursively into hap"
-	echo "output is current directory"
+	echo "convert *.{mov,mp4,m4v} recursively into hap"
+	echo "output is same directory as input"
 	echo "usage: $0 directory"
 	exit 1
 }
 
 # process one movie
 do_ffmpeg () {
-	out=${1##*/}
+	dirplusbase=${1%.*}
+	filename=${1##*/}
+	out=${dirplusbase}_hap.mov
 	ffmpeg -i "$1" -c:v hap -an "$out"
 }
 
 # find *.mov
 export -f do_ffmpeg
-find "$1" -name "*.mov" -exec bash -c 'do_ffmpeg "{}"' \;
+find "$1" \( -name "*.mov" -o -name "*.mp4" -o -name "*.m4v" \) -exec bash -c 'do_ffmpeg "{}"' \;
